f5fc5fafbfc39f1019b8b9964cd67664f49de057
Venktesh123 <8303531530venktesh@gmail.com>
2025-01-15 11:58:07 +0530

    ngr

--- controller/chatBotController.js
+++ controller/chatBotController.js
@@ -4,7 +4,7 @@ export const askQuestion = async (req, res) => {
   const { question } = req.body; // Corrected: Removed parentheses
   try {
     const response = await axios.post(
-      " https://2bd5-2409-40e3-4062-3443-f56d-49e1-69a2-6fde.ngrok-free.app/ask-question",
+      "https://96ef-2409-40e3-4062-3443-f56d-49e1-69a2-6fde.ngrok-free.app/ask-question",
       {
         question,
       }
